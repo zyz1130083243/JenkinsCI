df282ec8cc9378c8c7ad8529a3eee071863a39d9
ZYZ <11300083243@qq.com>
2023-05-28 18:42:49 +0800

    jenkins-ci

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -1,2 +1,17 @@
-package org.example.controller;public class TestController {
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "11111111111";
+    }
+
+
+
+
 }
